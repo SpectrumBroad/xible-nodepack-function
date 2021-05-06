cc684728098f920c0d203fe6ebc9fd995246b96d
Stefan de Jong <steve25070@gmail.com>
2021-05-06 14:48:54 +0200

    Support named variables as input

--- index.js
+++ index.js
@@ -2,19 +2,30 @@
 
 module.exports = (NODE) => {
   const inputsIn = NODE.getInputByName('inputs');
+
+  const variablesIn = NODE.getInputByName('variables');
+
   const triggerOut = NODE.getOutputByName('trigger');
 
   const triggerIn = NODE.getInputByName('trigger');
   triggerIn.on('trigger', async (conn, state) => {
     const inputs = await inputsIn.getValues(state);
+    const variables = await variablesIn.getValues(state);
+
     let values;
-    const fn = eval(`(function(inputs, trigger, state){${NODE.data.function}})`);
-    fn(inputs, () => {
-      state.set(NODE, {
-        values
-      });
-      triggerOut.trigger(state);
-    });
+    const fn = eval(`(function(inputs, variables, trigger, state){${NODE.data.function}})`);
+    fn(
+      inputs,
+      variables.reduce((acc, cur) => {
+        acc[cur.name] = cur.values; return acc;
+      }, {}),
+      () => {
+        state.set(NODE, {
+          values
+        });
+        triggerOut.trigger(state);
+      }
+    );
   });
 
   const valuesOut = NODE.getOutputByName('values');
